35525aa2c3d4b9b25933f73799acb4bfc7793e73
harshithraihars <harshithraiharsu@gmail.com>
2025-01-04 12:46:12 +0530

    please

---
+++ install_chromium.sh
@@ -0,0 +1,6 @@
+# install_chromium.sh
+apt-get update
+apt-get install -y chromium-browser fonts-liberation libappindicator3-1 libnspr4 libnss3 libx11-xcb1 libxcomposite1 libxdamage1 libxrandr2
+
+# Check the path of Chromium
+which chromium-browser
